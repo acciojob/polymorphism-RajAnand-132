7832d276f26902f6f8366c5f44458712ec70768f
Raj Anand <rajnandirctc@gmail.com>
2022-10-11 08:45:13 +0530

    Update in code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,25 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]) {
+
+        Product p = new Product();
+        System.out.println(p.product(2,3) +" " + p.product(2,3,5) + " " +p.product(4.0,5.1));
+
+    }
+
+    public static class Product {
+
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
 }
-
-
